2088a265a385d5807230546af002ae9451e22f89
jtdub <james.williams@packetgeek.net>
2021-03-06 18:18:44 -0600

    update deploy

--- deploy/docker.sh
+++ deploy/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker build -t networktocode/nautobot-lab:latest .
 docker push networktocode/nautobot-lab:latest
