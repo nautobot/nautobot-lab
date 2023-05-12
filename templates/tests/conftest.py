e8999c2e84112142b1904424ad04e321c1782cc5
James Williams <amascuba@gmail.com>
2023-05-12 18:15:13 -0500

    Update GHA CI

--- templates/tests/conftest.py
+++ templates/tests/conftest.py
@@ -1,10 +1,27 @@
+import subprocess
 import pynautobot
 import pytest
 
 
 @pytest.fixture
 def nautobot():
+    token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    create_token = [
+        "u=User.objects.get_or_create(username='demo', password='nautobot', is_staff=True, is_superuser=True)",
+        "u=u[0]",
+        "u.validated_save()",
+        f"t=Token.objects.get_or_create(key='{token}', user_id=u.pk, write_enabled=True)",
+        "t=t[0]",
+        "t.validated_save()"
+    ]
+    nautobot_shell = [
+        "nautobot-server",
+        "nbshell",
+        "--command",
+        "; ".join(create_token)
+    ]
+    subprocess.run(nautobot_shell, check=True)
     return pynautobot.api(
         url="http://localhost:8000",
-        token="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        token=f"{token}",
     )
