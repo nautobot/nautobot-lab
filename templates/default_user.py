cd4dcf80e83fad74d6aa3a6d3fc2b620025e8e38
James Williams <amascuba@gmail.com>
2023-05-29 00:31:57 +0000

    Fix typos in default user script

--- templates/default_user.py
+++ templates/default_user.py
@@ -4,31 +4,32 @@ import os
 import django
 import nautobot
 
-# Initialize Nautobot and Django Settings.
+print("Initialize Nautobot and Django Settings.")
 nautobot.setup()
 django.setup()
 
 from django.contrib.auth import get_user_model
 from nautobot.users.models import Token
 
-# Create User() model instance.
+print("Create User() model instance.")
 User = get_user_model()
 
+print(f"Create {os.getenv("NAUTOBOT_USERNAME")} account.")
 account = User(
     username=os.getenv("NAUTOBOT_USERNAME"),
     email=os.getenv("NAUTOBOT_EMAIL"),
     is_superuser=True,
     is_staff=True,
-    tokens=token
 )
 account.set_password(
     raw_password=os.getenv("NAUTOBOT_PASSWORD")
 )
 account.save()
 
+print(f"Create {os.getenv("NAUTOBOT_USERNAME")} API token.")
 token = Token(
     key=os.getenv("NAUTOBOT_TOKEN"),
     write_enabled=True,
-    user_id=account
+    user_id=account.pk
 )
 token.save()
